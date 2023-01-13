9944519ac907c0e3c1cb9e0c51893685ef43c313
swagata-codearts <swagata.codearts@gmail.com>
2023-01-13 19:22:51 +0530

    property details

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,55 @@
 // JavaScript Document
-jQuery('#custom-owl').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+// jQuery('#custom-owl').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// });
+
+  
+  
+jQuery('.count-number').each(function () {
+    jQuery(this).prop('Counter',0).animate({
+        Counter: jQuery(this).text()
+    }, {
+      
+      //chnage count up speed here
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            jQuery(this).text(Math.ceil(now));
         }
-    }
+    });
 });
+
+
+$(document).ready(function(){
+    var owl = $('.main-slider');
+      owl.owlCarousel({
+          autoplay: true,
+          autoplayTimeout: 4000,
+          loop: true,
+          items: 1,
+          center: true,
+          nav: true,
+          thumbs: true,
+          thumbImage: false,
+          thumbsPrerendered: true,
+          thumbContainerClass: 'owl-thumbs',
+          thumbItemClass: 'owl-thumb-item',
+          navText: ['<span class="prev">＜</span>','<span class="next">＞</span>'],
+      });
+  });
+  
+  
+  
